72e0f569dfb2c56c3945d5d0aa9a21ac10de67f1
trias <iaszaen@gmail.com>
2025-07-17 10:17:12 +0700

    first commit

--- pages/components/organisms/WeedingLocation/index.tsx
+++ pages/components/organisms/WeedingLocation/index.tsx
@@ -48,7 +48,7 @@ export default function WeedingLocation() {
               <h4>Graha Umbaran</h4>
               <h4>
                 Jl. Pangeran Cakrabuana No.102, Sendang, Kec. Sumber, Kabupaten
-                Cirebon, Jawa Barat 45611 tests
+                Cirebon, Jawa Barat 45611 testsssssss
               </h4>
             </div>
           </div>
